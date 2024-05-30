04a00a6cca70d91847e7b7a75d4a4ce654c24e11
Martin-Portefolie <n1tramkr1st1ansen@gmail.com>
2024-05-30 13:43:28 +0200

    added images > added browsepage > updated controller

--- src/Controller/DogController.php
+++ src/Controller/DogController.php
@@ -15,13 +15,13 @@ class DogController extends AbstractController
     public function homepage(): Response
     {
         $dogbreeds = [
-            ['name' => 'dalmatian'],
-            ['name' => 'shepherd dog'],
-            ['name' => 'poodle'],
-            ['name' => 'boxer'],
-            ['name' => 'old english shepherd'],
-            ['name' => 'newfoundland'],
-            ['name' => 'golden retriever'],
+            ['name' => 'dalmatian' , 'image' => '/images/dalma.webp' , 'size' => 'medium-size' ],
+            ['name' => 'shepherd dog' , 'image' => '/images/shepherd.jpeg' , 'size' => 'medium-size'],
+            ['name' => 'poodle' , 'image' => '/images/poodle.jpeg' , 'size' => 'medium-size'],
+            ['name' => 'boxer', 'image' => '/images/Boxerdog.webp' , 'size' => 'medium-size'],
+            ['name' => 'old english shepherd' , 'image' => '/images/old-english-sheepdog.webp' , 'size' => 'large-size'],
+            ['name' => 'newfoundland' , 'image' => '/images/newfoundland.webp', 'size' => 'large-size'],
+            ['name' => 'golden retriever' , 'image' => '/images/golden.jpeg' , 'size' => 'medium-size'],
         ];
         return $this->render('dogs/homepage.html.twig', ['title' => 'Chose a dog',  'dogbreeds' => $dogbreeds ]);
 
@@ -30,13 +30,12 @@ class DogController extends AbstractController
     #[Route('/dog_breed/{slug}', 'breed_dog')]
     public function dogbreed(string $slug = null): Response
     {
-        if($slug) {
-            $title = 'Breed:'.u(str_replace('-', ' ', $slug))->title(true);
-        }else {
-            $title = 'alle breeds selected';
-        }
 
-        return new Response($title);
+
+
+        $breeds = $slug ? $title = 'Breed:'.u(str_replace('-', ' ', $slug))->title(true): $title = 'alle breeds selected';
+        return $this->render('dogs/browse.html.twig', ['title' => 'browse for dogs', 'breeds' => $breeds]);
+
     }
 
 }
