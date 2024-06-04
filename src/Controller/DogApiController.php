006f697d75966b3aeb529c56033e85cf8128d729
Martin-Portefolie <n1tramkr1st1ansen@gmail.com>
2024-06-04 13:42:33 +0200

    added a Controller > updated a controller

---
+++ src/Controller/DogApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DogApiController extends AbstractController
+{
+    #[Route('/api/dogs/{id<\d+>}', methods: ['GET'])]
+public function getDog(int $id, LoggerInterface $log ):Response
+{
+$dog =[
+    'id' => $id,
+     'breed' => 'labrador',
+    'size' => 'medium',
+    'pelt' => 'short',
+];
+
+$log->info(' Returnere API response for hunde racer {dog}', [
+    'dog' => $id
+]);
+
+return $this->json($dog);
+}
+}
